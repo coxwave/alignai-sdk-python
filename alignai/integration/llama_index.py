53f67ca1d328fce18d346be0ce4fa3ca03dfc2a0
Changjin Lee <changjinlee@Changjins-MacBook-Pro.local>
2023-11-17 11:23:17 +0900

    Dynamic attributes for ChatMemoryBuffer

--- alignai/integration/llama_index.py
+++ alignai/integration/llama_index.py
@@ -14,6 +14,7 @@ try:
     if version.parse(llama_index_version) < version.parse("0.8.4"):
         raise ImportError("You must install llama_index>=0.8.4 to use Align AI integrated ChatMemory.")
     BaseMemory = importlib.import_module("llama_index.memory.types").BaseMemory
+    ChatMemoryBuffer = importlib.import_module("llama_index.memory.chat_memory_buffer").ChatMemoryBuffer
     llm_base = importlib.import_module("llama_index.llms.base")
     MessageRole = llm_base.MessageRole
     ChatMessage = llm_base.ChatMessage
@@ -30,31 +31,32 @@ class UserInfo(TypedDict, total=False):
     display_name: str | None
 
 
-class ImpactionChatMemory(BaseMemory):
+class ChatMemory(BaseMemory):
     chat_memory: BaseMemory
-    impaction_client: AlignAI
+    align_client: AlignAI
     session_id: str
     user_id: str
     assistant_id: str = DEFAULT_ASSISTANT_ID
     user_info: UserInfo | None = None
+    dynamic_attrs: dict
 
-    class Config:  # for custom type ImpactionAI against pydantic validation.
+    class Config:  # for custom type AlignAI against pydantic validation.
         arbitrary_types_allowed = True
 
     def __init__(
         self,
         chat_memory: BaseMemory,
-        impaction_client: AlignAI,
+        align_client: AlignAI,
         session_id: str,
         user_id: str,
         assistant_id: str = DEFAULT_ASSISTANT_ID,
         user_info: UserInfo | None = None,
     ):
-        """Initialize ImpactionChatMemory. When initialized, open_session event is emitted to ImpactionAI. If user_info is provided, identify_user event is also emitted.
+        """Initialize ChatMemory. When initialized, open_session event is emitted to alignai. If user_info is provided, identify_user event is also emitted.
 
         Args:
             chat_memory (BaseMemory): Chat memory from LlamaIndex. Reference: https://docs.llamaindex.ai/en/stable/api_reference/memory.html
-            impaction_client (AlignAI): impaction.ai SDK client.
+            align_client (AlignAI): alignai SDK client.
             session_id (str): Session ID.
             user_id (str): User Id.
             assistant_id (str, optional): Assistant ID. Defaults to "DEFAULT".
@@ -62,15 +64,20 @@ class ImpactionChatMemory(BaseMemory):
         """  # noqa: E501
         super().__init__(
             chat_memory=chat_memory,
-            impaction_client=impaction_client,
+            align_client=align_client,
             session_id=session_id,
             user_id=user_id,
             assistant_id=assistant_id,
             user_info=user_info,
+            dynamic_attrs={},
         )
-        self.impaction_client.open_session(
-            session_id=self.session_id, user_id=self.user_id, assistant_id=self.assistant_id
-        )
+
+        if isinstance(chat_memory, ChatMemoryBuffer):
+            self.dynamic_attrs["token_limit"] = chat_memory.token_limit
+            self.dynamic_attrs["tokenizer_fn"] = chat_memory.tokenizer_fn
+            self.dynamic_attrs["chat_history"] = chat_memory.chat_history
+
+        self.align_client.open_session(session_id=self.session_id, user_id=self.user_id, assistant_id=self.assistant_id)
         if self.user_info:
             self.identify_user(
                 email=self.user_info.get("email", None),
@@ -80,20 +87,25 @@ class ImpactionChatMemory(BaseMemory):
                 display_name=self.user_info.get("display_name", None),
             )
 
+    def __getattr__(self, name):
+        if name in self.dynamic_attrs:
+            return self.dynamic_attrs[name]
+        raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
+
     @classmethod
     def from_defaults(cls, chat_history: list[ChatMessage] | None = None, llm: LLM | None = None):
-        """ImpactionChatMemory does not support 'from_defaults' constructor. Please use the constructor directly."""
+        """ChatMemory does not support 'from_defaults' constructor. Please use the constructor directly."""
         raise NotImplementedError(
-            "from_defaults is not intended to be used with ImpactionChatMemory. Please use the constructor directly."
+            "from_defaults is not intended to be used with ChatMemory. Please use the constructor directly."
         )
 
-    def get(self) -> list[ChatMessage]:
+    def get(self, **kwargs: any) -> list[ChatMessage]:
         """Get chat history.
 
         Returns:
             list[ChatMessage]: Chat history.
         """
-        return self.chat_memory.get()
+        return self.chat_memory.get(**kwargs)
 
     def get_all(self) -> list[ChatMessage]:
         """Get all chat history.
@@ -104,13 +116,13 @@ class ImpactionChatMemory(BaseMemory):
         return self.chat_memory.get_all()
 
     def put(self, message: ChatMessage) -> None:
-        """Put chat history. If the message is user or assistant message, create_message event will be emitted to ImpactionAI.
+        """Put chat history. If the message is user or assistant message, create_message event will be emitted to align.ai.
 
         Args:
             message (ChatMessage): Chat message.
         """  # noqa: E501
         if message.role in [MessageRole.USER, MessageRole.ASSISTANT] and message.content is not None:
-            self.impaction_client.create_message(
+            self.align_client.create_message(
                 session_id=self.session_id,
                 message_index=self._next_message_idx,
                 role=ROLE_USER if message.role == MessageRole.USER else ROLE_ASSISTANT,
@@ -138,7 +150,7 @@ class ImpactionChatMemory(BaseMemory):
         create_time: datetime | None = None,
         display_name: str | None = None,
     ) -> None:
-        """Send identify_user event to ImpactionAI. The user_id provided upon initialization will be used.
+        """Send identify_user event to align.ai. The user_id provided upon initialization will be used.
 
         Args:
             email (str | None, optional): User email address. Defaults to None.
@@ -147,7 +159,7 @@ class ImpactionChatMemory(BaseMemory):
             create_time (datetime | None, optional): User creation time. Defaults to None.
             display_name (str | None, optional): User display name. Defaults to None.
         """  # noqa: E501
-        self.impaction_client.identify_user(
+        self.align_client.identify_user(
             user_id=self.user_id,
             email=email,
             ip=ip,
@@ -157,8 +169,8 @@ class ImpactionChatMemory(BaseMemory):
         )
 
     def close(self) -> None:
-        """Close the ImpactionAI session. Do not send additional message after calling this method."""
-        self.impaction_client.close_session(session_id=self.session_id)
+        """Close the AlignAI session. Do not send additional message after calling this method."""
+        self.align_client.close_session(session_id=self.session_id)
 
     @property
     def _messages(self) -> list[ChatMessage]:
