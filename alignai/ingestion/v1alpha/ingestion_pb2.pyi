d179d729fe8d168a7cebcc1d716c489794bdd3ec
nyanxyz <wl77886@gmail.com>
2024-03-21 18:29:59 +0900

    fix: invalid import paths

--- alignai/ingestion/v1alpha/ingestion_pb2.pyi
+++ alignai/ingestion/v1alpha/ingestion_pb2.pyi
@@ -1,10 +1,16 @@
 from google.api import field_behavior_pb2 as _field_behavior_pb2
 from google.protobuf import empty_pb2 as _empty_pb2
-from ingestion.v1alpha import event_pb2 as _event_pb2
+from alignai.ingestion.v1alpha import event_pb2 as _event_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+from typing import (
+    ClassVar as _ClassVar,
+    Iterable as _Iterable,
+    Mapping as _Mapping,
+    Optional as _Optional,
+    Union as _Union,
+)
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
@@ -14,4 +20,6 @@ class CollectEventsRequest(_message.Message):
     EVENTS_FIELD_NUMBER: _ClassVar[int]
     request_id: str
     events: _containers.RepeatedCompositeFieldContainer[_event_pb2.Event]
-    def __init__(self, request_id: _Optional[str] = ..., events: _Optional[_Iterable[_Union[_event_pb2.Event, _Mapping]]] = ...) -> None: ...
+    def __init__(
+        self, request_id: _Optional[str] = ..., events: _Optional[_Iterable[_Union[_event_pb2.Event, _Mapping]]] = ...
+    ) -> None: ...
