7e6299e3d703674b5f674a9a59aa09019797c7db
Dongeon Kim <dongeon.kim@tryalign.ai>
2024-10-08 08:50:43 +0000

    remove pendulum usage

--- alignai/client.py
+++ alignai/client.py
@@ -2,9 +2,8 @@ from __future__ import annotations
 
 import os
 import uuid
-from datetime import datetime
+from datetime import datetime, timezone
 
-import pendulum
 from ingestion.v1alpha.event_pb2 import Event, EventProperties
 
 from alignai.api_client import APIClient
@@ -100,7 +99,7 @@ class AlignAI:
         open_session_event = Event(
             id=uuid.uuid4().hex,
             type=EventTypes.SESSION_OPEN,
-            create_time=datetime_to_timestamp(pendulum.now()),
+            create_time=datetime_to_timestamp(datetime.now(timezone.utc)),
             properties=EventProperties(
                 session_properties=EventProperties.SessionProperties(**session_properties_args),
                 custom_properties=serialize_custom_properties(custom_properties) if custom_properties else None,
@@ -120,7 +119,7 @@ class AlignAI:
         close_session_event = Event(
             id=uuid.uuid4().hex,
             type=EventTypes.SESSION_CLOSE,
-            create_time=datetime_to_timestamp(pendulum.now()),
+            create_time=datetime_to_timestamp(datetime.now(timezone.utc)),
             properties=EventProperties(session_properties=EventProperties.SessionProperties(session_id=session_id)),
             project_id=self.project_id,
         )
@@ -172,7 +171,7 @@ class AlignAI:
         identify_user_event = Event(
             id=uuid.uuid4().hex,
             type=EventTypes.USER_RECOGNIZE,
-            create_time=datetime_to_timestamp(pendulum.now()),
+            create_time=datetime_to_timestamp(datetime.now(timezone.utc)),
             properties=EventProperties(
                 user_properties=EventProperties.UserProperties(**user_properties_args),
                 custom_properties=serialize_custom_properties(custom_properties) if custom_properties else None,
@@ -211,7 +210,7 @@ class AlignAI:
         create_message_event = Event(
             id=uuid.uuid4().hex,
             type=EventTypes.MESSAGE_CREATE,
-            create_time=datetime_to_timestamp(pendulum.now()),
+            create_time=datetime_to_timestamp(datetime.now(timezone.utc)),
             properties=EventProperties(
                 message_properties=EventProperties.MessageProperties(
                     session_id=session_id,
@@ -249,7 +248,7 @@ class AlignAI:
         create_feedback_event = Event(
             id=uuid.uuid4().hex,
             type=EventTypes.MESSAGE_CREATE,
-            create_time=datetime_to_timestamp(pendulum.now()),
+            create_time=datetime_to_timestamp(datetime.now(timezone.utc)),
             properties=EventProperties(
                 feedback_properties=EventProperties.FeedbackProperties(
                     session_id=session_id,
@@ -285,7 +284,7 @@ class AlignAI:
         create_feedback_event = Event(
             id=uuid.uuid4().hex,
             type=EventTypes.MESSAGE_CREATE,
-            create_time=datetime_to_timestamp(pendulum.now()),
+            create_time=datetime_to_timestamp(datetime.now(timezone.utc)),
             properties=EventProperties(
                 feedback_properties=EventProperties.FeedbackProperties(
                     session_id=session_id,
